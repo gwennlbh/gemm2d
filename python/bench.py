80d8650d50709af5866890043363c1a7c077b224
LE BIHAN Gwenn <elebihan@rondoudou.enseeiht.fr>
2025-01-31 15:27:18 +0100

    benchmarks

--- python/bench.py
+++ python/bench.py
@@ -1,5 +1,7 @@
 import subprocess
 import pandas as pd
+from pathlib import Path
+from time import time
 import os
 
 # Fonction pour exécuter une commande MPI
@@ -30,9 +32,11 @@ def run_mpi_command(m, n, k, b, p, q, algo, la, niter, output_file="bench.csv"):
     # Exécuter la commande et récupérer la sortie
     try:
         print(' '.join(command))
+        start = time()
         result = subprocess.run(command, capture_output=True, text=True, check=True)
+        duration = time() - start
         gflops = parse_output(result.stdout)
-        save_to_csv(m, n, k, b, p, q, algo, la, gflops, output_file)
+        save_to_csv(m, p, q, algo, la, gflops, output_file, duration)
     except subprocess.CalledProcessError as e:
         print(e.stdout)
         print(f"Erreur lors de l'exécution de {algo}: {e.stderr}")
@@ -45,12 +49,15 @@ def parse_output(output):
     return None
 
 # Fonction pour sauvegarder les résultats dans un fichier CSV
-def save_to_csv(m, n, k, b, p, q, algo, la, gflops, output_file):
+def save_to_csv(m, p, q, algo, la, gflops, output_file, duration):
     df = pd.DataFrame([{
-        "m": m, "n": n, "k": k, "b": b,
+        "m": m,
         "p": p, "q": q, "algo": algo,
-        "lookahead": la, "gflops": gflops
+        "lookahead": la, "gflops": gflops,
+        "process_count": p * q,
+        "duration": duration
     }])
+
     if not os.path.exists(output_file):
         df.to_csv(output_file, index=False)
     else:
@@ -61,13 +68,14 @@ if __name__ == "__main__":
     b = 256
     niter = 5
     output_file = "bench.csv"
+    Path("bench.csv").unlink(missing_ok=True)
 
     # Configurations des paramètres
     for p in [2, 4]:
         for q in [2, 4]:
             for i in [4, 8, 12]:
                 n = m = k = i * b
-                for algo in ["p2p", "bcast"]:
-                    run_mpi_command(m, n, k, b, p, q, algo, la=0, niter=niter, output_file=output_file)
-                for la in range(1, k // b + 1):
-                    run_mpi_command(m, n, k, b, p, q, "p2p-i-la", la, niter, output_file)
+                run_mpi_command(m, n, k, b, p, q, "bcast", la=0, niter=niter, output_file=output_file)
+                run_mpi_command(m, n, k, b, p, q, "p2p", la=5, niter=niter, output_file=output_file)
+                # for la in range(1, k // b + 1):
+                run_mpi_command(m, n, k, b, p, q, "p2p-i-la", la=2, niter=niter, output_file=output_file)
---
+++ python/graph.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def read_timings_from_csv(filename = "bench.csv"):
+    return pd.read_csv(filename)
+
+def plot_timings(df, x, y, style,  title, xlabel, ylabel, output_file):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=df, x=x, y=y, style=style)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid()
+    plt.savefig(output_file)
+    plt.show()
+
+if __name__ == "__main__":
+    df = read_timings_from_csv()
+    for algo in {"p2p", "bcast", "p2p-i-la"}:
+        plot_timings(df[df["algo"] == algo], "process_count", "duration", "m", f"Performance de {algo}", "Nombre de processus", "Temps [s]", f"{algo}.png")
+    # plot_timings(df, "process_count", "duration", "algo", "lookahead", "Performance des algorithmes", "Taille des matrices", "GFLOPS", "performance.png")
